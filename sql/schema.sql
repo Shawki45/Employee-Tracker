52898c589020cac7a9181deb944166f55dcb46ca
Shawki45 <shawkihaifa@yahoo.com>
2024-02-01 14:33:42 -0500

    added the schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,44 +1,32 @@
-/ Simple basic start up approach to Employee Tracker
+DROP DATABASE IF EXISTS Employee_Tracker;
+CREATE DATABASE Employee_Tracker;
+USE Employee_Tracker;
 
-// get inquirer 
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARVHAR(30) NOT NULL
+)
 
+CREATE TABLE Role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT ,
+  foreign key (department_id)
+  references department (id)
+  on delete set NULL
+);
 
-/* prompt with -- 
-  List:
-  - view employees
-  - view departments
-  - view roles
-  - add employee
-  - change role
-  - add role
-*/
-
-// This will have the initial prompt for "What do you want to do?"
-start();
-
-/** ToDo --eventually I have a Join that shows the department name */
-function getAllEmployees(params) {
-    db.query(`select * from employees`,(err,results)=>{
-        console.table(results);
-        start();
-    });
-}
-
-function getAllRoles(params) {
-    db.query(`select * from roles`,(err,results)=>{
-        console.table(results);
-        start();
-    })
-}
-// function to query fro all employees and log the table
-switch (key) {
-    case `view employees`:
-        getAllEmployees();
-        break;
-    case `view roles`:
-    getAllRoles();
-    break;
-
-    default:
-        break;
-}
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT Null,
+    last_name VARCHAR(30) ,
+    role_id INT,
+    foreign key (role_id)
+    references role (id)
+  on delete set NULL,
+  manager_id INT,
+  foreign key (manager_id)
+  referwnces employee(id)
+  on delete set NULL
+);
